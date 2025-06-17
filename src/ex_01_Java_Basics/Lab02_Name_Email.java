c9a69eb24d57c513f0d509bd3f995b2fd5e52838
NuruddinSyed18 <nuruddinsyed18@gmail.com>
2025-06-18 01:50:34 +0530

    5th june task

---
+++ src/ex_01_Java_Basics/Lab02_Name_Email.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class Lab02_Name_Email {
+    public static void main(String[] args) {
+        System.out.println("NuruddinSyed18, nuruddinsyed18@gmail.com");
+    }
+}
--- src/ex_01_Java_Basics/Lab04_Details.java
+++
@@ -1,7 +0,0 @@
-package ex_01_Java_Basics;
-
-public class Lab04_Details {
-    public static void main(String[] args) {
-        System.out.println("Syed Nuruddin");
-    }
-}

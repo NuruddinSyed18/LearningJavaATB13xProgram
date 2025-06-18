3211071faa2ff68313c130409809569a58de8104
NuruddinSyed18 <nuruddinsyed18@gmail.com>
2025-06-18 08:52:42 +0530

    18th June Practice Code

---
+++ src/ex_01_Java_Basics/Lab04_CountryName.java
@@ -0,0 +1,8 @@
+package ex_01_Java_Basics;
+
+public class Lab04_CountryName {
+    public static void main(String[] args){
+        System.out.println("Country: India");
+        System.out.println("Place: Narasaraopet");
+    }
+}

5a97aa731289086c18b56d715bca0f4dbdf49e51
NuruddinSyed18 <nuruddinsyed18@gmail.com>
2025-06-18 08:45:04 +0530

    18th June Practice Code

---
+++ src/ex_01_Java_Basics/Lab03_Personal_Details.java
@@ -0,0 +1,11 @@
+package ex_01_Java_Basics;
+
+public class Lab03_Personal_Details {
+    public static void main(String[] args){
+        System.out.println("Name: Syed Nuruddin");
+        System.out.println("Age: 24");
+        System.out.println("DOB: Jan:01:2001");
+
+    }
+
+}

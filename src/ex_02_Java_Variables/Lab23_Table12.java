0328fea1d56fc8b740eb9f03355786d21cadf280
NuruddinSyed18 <nuruddinsyed18@gmail.com>
2025-09-23 20:54:44 +0530

    10th june task

---
+++ src/ex_02_Java_Variables/Lab23_Table12.java
@@ -0,0 +1,48 @@
+package ex_02_Java_Variables;
+
+public class Lab23_Table12 {
+    public static void main(String [] args){
+        int a = 12;
+        int b = 1;
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%d x %d = %d",a,b,a*b);
+        System.out.println();
+        b++;
+
+    }
+}
